7cba9997ad71aa3d0a80f6372f1c8ab2c3d545e8
Male Michael <michael.male@andela.com>
2019-03-18 15:00:38 +0300

    separate api documentation from server setup

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,33 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
-import * as swaggerJSDoc from 'swagger-jsdoc';
 
 import * as userAuthentication from './routes/authentication/user_authentication.router';
+import ApiDocumentation from './api_documentation'
+import { Utilities } from './utilities/utilities';
+import config from './config';
 
-/**
- * Swagger specification
- */
-const swagger_options = {
-  definition: {
-    swagger: '2.0',
-    info: {
-      title: 'SMS Management Application API',
-      version: '1.0.0',
-      description: 'SMS Management Application API',
-    },
-    host: 'localhost:1337',
-    basePath: '/',
-  },
-  apis: ['./**/*.router.js', './**/*.router.ts'],
-};
-
-const swaggerSpec = swaggerJSDoc(swagger_options);
+const apiDocumentation = new ApiDocumentation();
 
 /**
  * Setup express server
  */
 const app = express();
-const port = process.env.PORT || 1337;
+const port = config.PORT;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/authentication', userAuthentication.getUser());
-app.use('/authentication', userAuthentication.userSignUp());
+app.use(apiDocumentation.swaggerDocumentation())
+app.use(userAuthentication.getUser());
+app.use(userAuthentication.userSignUp());
 
 app.get( '/', function ( req, res )
 {
-  res.send( 'Welcome to my API' );
+  const hostUrl = `${req.protocol}://${Utilities.getHostUrl(req, port)}`
+  res.send( `Welcome to the SMS Application API. You can access the API documentation at <a href="${hostUrl}/api-docs/">here</a>` );
 })
-
-app.get('/swagger.json', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-});
 
 app.listen( port, function ()
 {
