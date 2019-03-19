cccf5193357cd5df4de363b2460b183d9a316ab8
Male Michael <michael.male@andela.com>
2019-03-19 10:59:03 +0300

    add integration to mongo db (#3)

--- src/routes/authentication/user_authentication.router.ts
+++ src/routes/authentication/user_authentication.router.ts
@@ -1,49 +1,43 @@
 import * as express from 'express';
+import UserModel from '../../models/user.model';
 
-let user = [];
+export default class UserAuthentication{
+  router: express.Router;
+  constructor(){
+    this.router = express.Router();
+  }
 
-export function userSignUp(): express.Router{
-  var router = express.Router();
-  router.post('/registerUser', (req, res) => {
-    user.push(req.body);
-    console.log(`User: ${user}`);
-    return res.status(201).send(JSON.stringify(user))
-  })
-  return router;
-}
+  userSignUp(): express.Router{
+    this.router.post('/registerUser', (req, res) => {
+      const user = new UserModel(req.body);
+      user.save();
+      return res.status(201).send(JSON.stringify(user))
+    })
+    return this.router;
+  }
 
-/**
- * @swagger
- * definition:
- *   users:
- *     properties:
- *       name:
- *         type: string
- *       phone_number:
- *         type: string
- */
-
-/**
- * @swagger
- * /authentication/getUser:
- *   get:
- *     tags:
- *       - users
- *     description: Gets all users
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Successfully deleted
- *         schema:
- *           $ref: '#/definitions/users'
- */
-export function getUser(): express.Router{
-  var router = express.Router();
-  router.get('/getUser', (req, res) => {
-    res.json({user: user})
-    res.status(200);
-  })
-  debugger;
-  return router;
+  /**
+   * @swagger
+   * /getUser:
+   *   get:
+   *     tags:
+   *       - users
+   *     description: Gets all users
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Successfully deleted
+   *         schema:
+   *           $ref: '#/definitions/users'
+   */
+  getUser(): express.Router{
+    this.router.get('/getUser', (req, res) => {
+      UserModel.find((err, values)=> {
+        if(err) res.status(500).send(err)
+        else res.json(values);
+      })
+    })
+    return this.router;
+  }
 }
