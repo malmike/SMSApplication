0618d4f175563d493a084c23fe91ab495065c6df
Male Michael <michael.male@andela.com>
2019-04-26 14:47:27 +0300

    add user authentication routes to express server

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
 
-import UserAuthentication from './routes/authentication/user_authentication.router';
-import ApiDocumentation from './app_configurations/api_documentation'
+import UserRegistrationRoutes from './routes/authentication/user_registration.router';
+import ApiDocumentation from './app_configurations/api_documentation';
 import { Utilities } from './utilities/utilities';
 import config from './app_configurations/config';
 
@@ -15,11 +15,11 @@ import config from './app_configurations/config';
 
 export default class ServerSetup{
   app = express();
-  private userAuthentication: UserAuthentication;
+  private userRegistrationRoutes: UserRegistrationRoutes;
   private apiDocumentation: ApiDocumentation;
 
   constructor(){
-    this.userAuthentication = new UserAuthentication();
+    this.userRegistrationRoutes = new UserRegistrationRoutes();
     this.apiDocumentation = new ApiDocumentation();
   }
 
@@ -30,16 +30,14 @@ export default class ServerSetup{
     this.app.use(bodyParser.json());
     this.app.use(express.static(path.join(__dirname, 'public')));
 
-    this.app.use(this.apiDocumentation.swaggerDocumentation())
-    this.app.use(this.userAuthentication.getUser());
-    this.app.use(this.userAuthentication.userSignUp());
-
     this.app.get( '/', function ( req, res )
     {
       const hostUrl = `${req.protocol}://${Utilities.getHostUrl(req, port)}`
       res.send( `Welcome to the SMS Application API. You can access the API documentation at <a href="${hostUrl}/api-docs/">here</a>` );
     })
-
+    this.app.use(this.apiDocumentation.swaggerDocumentation());
+    this.app.use(this.userRegistrationRoutes.userSignUp());
+    this.app.use(this.userRegistrationRoutes.getUser());
     this.app.listen( port, function ()
     {
       console.log( 'Running on port: ' + port );
